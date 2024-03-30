2e28bf5c8f4804f36c83fa571329dde04b4c0992
Geetanjali Gomber <geetanjali.gomber@thoughtworks.com>
2024-03-30 13:07:46 +0530

    Use reduce to add numbers using lambda

--- src/FP02.java
+++ src/FP02.java
@@ -1,18 +1,22 @@
 import java.util.List;
 
-/* 1. SUm of all numbers
-* */
+/* 1. Sum of all numbers
+ * */
 public class FP02 {
     public static void main(String[] args) {
         List<Integer> list = List.of(12, 6, 34, 89, 56, 45);
         System.out.println(sum(list));
     }
-    //a=accumulator(containing sum till now) b=next number to be added
-    public static int add(int a, int b){
-        return a+b;
+
+    //a=aggregate(containing sum till now) b=next number to be added
+    public static int add(int aggregate, int nextNumber) {
+        return aggregate + nextNumber;
     }
+
     private static int sum(List<Integer> list) {
         //0 to initialise the value
-        return list.stream().reduce(0, FP02::add);
+        //return list.stream().reduce(0, FP02::add);
+        //since we can also use lamba exp where we can pass params and compute result and hence using it, hence we will use it
+        return list.stream().reduce(0, (x,y)->x+y);
     }
 }
