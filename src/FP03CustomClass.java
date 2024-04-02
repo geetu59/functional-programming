a661c0c6f60e7f6ca699cdeda794fac4d90a0edd
Geetanjali Gomber <geetanjali.gomber@thoughtworks.com>
2024-04-02 14:45:46 +0530

    comparing and sorting usage

--- src/FP03CustomClass.java
+++ src/FP03CustomClass.java
@@ -1,13 +1,16 @@
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Getter;
 import lombok.ToString;
 
+import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
+
 /*We are trying to use lambda expressions on custom objects
-* 1. allMatch-> if the whole list fulfils the condition
-* 2. anyMatch-> if any item of the list fulfils the condition
-* 3. noneMatch -> if none of the items fulfils the condition*/
+ * 1. allMatch-> if the whole list fulfils the condition
+ * 2. anyMatch-> if any item of the list fulfils the condition
+ * 3. noneMatch -> if none of the items fulfils the condition
+ * 4. Comparator.comparing -> to sort values*/
 @AllArgsConstructor
 @Getter
 @ToString
@@ -21,18 +24,27 @@ class Course {
 public class FP03CustomClass {
     public static void main(String[] args) {
         List<Course> courses = List.of(new Course("Spring", "Framework", 98, 20000),
-                new Course("Spring Boot", "Framework", 95, 20000),
-                new Course("API", "Microservices", 97, 20000),
-                new Course("Microservices", "Microservices", 96, 20000),
-                new Course("FullStack", "FullStack", 91, 20000),
-                new Course("AWS", "Cloud", 92, 20000),
-                new Course("Azure", "Cloud", 99, 20000),
+                new Course("Spring Boot", "Framework", 95, 18000),
+                new Course("API", "Microservices", 97, 22000),
+                new Course("Microservices", "Microservices", 96, 25000),
+                new Course("FullStack", "FullStack", 91, 14000),
+                new Course("AWS", "Cloud", 92, 21000),
+                new Course("Azure", "Cloud", 99, 21000),
                 new Course("Docker", "Cloud", 92, 20000),
                 new Course("Kubernetes", "Cloud", 91, 20000));
 
-        System.out.println(courses.stream().allMatch(course -> course.getReviewScore()>95));
-        System.out.println(courses.stream().anyMatch(course -> course.getReviewScore()>95));
-        System.out.println(courses.stream().noneMatch(course -> course.getReviewScore()>95));
+        System.out.println(courses.stream()
+                .allMatch(course -> course.getReviewScore() > 95));
+        System.out.println(courses.stream()
+                .anyMatch(course -> course.getReviewScore() > 95));
+        System.out.println(courses.stream()
+                .noneMatch(course -> course.getReviewScore() > 95));
+
+        System.out.println(courses.stream()
+                .sorted(Comparator.comparing(Course::getNumberOfStudents)
+                        .thenComparing(Course::getReviewScore)
+                        .reversed())
+                .collect(Collectors.toList()));
     }
 
 }
